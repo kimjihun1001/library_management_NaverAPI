058e3367bfd1a5ff6862b39974783e0d2b997c2d
Kim Ji Hoon <kimjihun1001@gmail.com>
2021-11-16 18:36:44 +0900

    추가: 1. NaverSearchBook 파라미터 변경: 검색 분야, 검색어, 검색 결과를 몇 개씩 볼지 2. NaverSearchBook 내부 기능 추가: 방향키를 사용하여 검색 결과 페이지 넘기기

--- library_management_NaverAPI/GetNaverAPI.cs
+++ library_management_NaverAPI/GetNaverAPI.cs
@@ -8,56 +8,101 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Text.RegularExpressions;
 
 class NaverAPI
 {
-    public void NaverSearchBook(string searchWord)
+    public void NaverSearchBook(string searchField, string searchWord, string displayNumber)
     {
-        const string NAVER_DISPLAY_STRING = "&display=";
         const string NAVER_ID = "xQKhYDcaW4DZ5JNHdmXJ";
         const string NAVER_SECRET = "869iq_F6ep";
         const string NAVER_URL = "https://openapi.naver.com/v1/search/book_adv.xml";
-        const string NAVER_SearchTitle = "?d_titl=";
+        const string NAVER_SearchForTitle = "?d_titl=";
+        const string NAVER_SearchForAuthor = "?d_auth=";
+        const string NAVER_DISPLAY_STRING = "&display=";
 
-        WebRequest request;
-        WebResponse response;
-        Stream stream;
-        XmlNode firstNode;
-        XmlNode secondNode;
-        XmlDocument xmlDocument = new XmlDocument();
-        XmlNodeList xmlNodeList;
-
-        XmlNode totalNode;
-
-        string url = NAVER_URL + NAVER_SearchTitle + searchWord + NAVER_DISPLAY_STRING + "13" + "&start=1";
-        request = (HttpWebRequest) WebRequest.Create(url);
-
-        request.Headers.Add("X-Naver-Client-Id", NAVER_ID);
-        request.Headers.Add("X-Naver-Client-Secret", NAVER_SECRET);           //api 접근
-
-        response = (HttpWebResponse)request.GetResponse();
-
-        stream = response.GetResponseStream();
-
-        xmlDocument.Load(stream);
-
-        firstNode = xmlDocument.SelectSingleNode("rss");
-        secondNode = firstNode.SelectSingleNode("channel");
-        xmlNodeList = secondNode.SelectNodes("item");
-
-        totalNode = secondNode.SelectSingleNode("total");
-
-        if (totalNode.InnerText == "0")
+        string url;
+        int page = 1;
+        bool IsThereResult = true;
+        
+        while (true)
         {
-            Console.WriteLine("검색 결과가 없습니다.");
-        }
-        else
-        {
-            foreach (XmlNode xmlNode in xmlNodeList)
+            if (searchField == "title")
             {
-                Console.WriteLine(xmlNode.SelectSingleNode("title").InnerText);  //책 출력.
+                url = NAVER_URL + NAVER_SearchForTitle + searchWord + NAVER_DISPLAY_STRING + displayNumber + "&start=" + page.ToString();
+            }
+            else if (searchField == "author")
+            {
+                url = NAVER_URL + NAVER_SearchForAuthor + searchWord + NAVER_DISPLAY_STRING + "20" + "&start=1";
+            }
+            else
+            {
+                url = "";
+                Console.WriteLine("검색 분야를 잘못 설정했습니다.");
+            }
+
+            // 요청
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            // API 접근
+            request.Headers.Add("X-Naver-Client-Id", NAVER_ID);
+            request.Headers.Add("X-Naver-Client-Secret", NAVER_SECRET);
+
+            // 응답 받기
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Xml 파일 받아오기
+            Stream stream = response.GetResponseStream();
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(stream);
+            //Console.WriteLine(xmlDocument.InnerXml);
+
+            // Xml 파일 분해하기
+            XmlNode firstNode = xmlDocument.SelectSingleNode("rss");
+            XmlNode secondNode = firstNode.SelectSingleNode("channel");
+            XmlNodeList xmlNodeList = secondNode.SelectNodes("item");
+            // 검색 결과의 갯수
+            XmlNode SearchResultCountNode = secondNode.SelectSingleNode("total");
+
+            if (SearchResultCountNode.InnerText == "0")
+            {
+                IsThereResult = false;
+                Console.WriteLine("검색 결과가 없습니다.");
+            }
+            else
+            {
+                foreach (XmlNode xmlNode in xmlNodeList)
+                {
+                    Console.WriteLine(xmlNode.SelectSingleNode("title").InnerText);
+                    //if (xmlNode.SelectSingleNode("discount").InnerText == "")   // 왠지 모르지만 null로 하면 안되길래 ""로 함.
+                    //{
+                    //    Console.WriteLine(xmlNode.SelectSingleNode("title").InnerText); // 책 제목
+                    //    Console.WriteLine("할인 가격 없음");
+                    //}
+                }
+
+            }
+
+            if (Console.ReadKey().Key == ConsoleKey.RightArrow)
+            {
+                if (IsThereResult == true)
+                    page += int.Parse(displayNumber);
+                else
+                    Console.WriteLine("더 이상 페이지가 없습니다.");
+            }
+            else if (Console.ReadKey().Key == ConsoleKey.LeftArrow)
+            {
+                if (page != 1)
+                    page -= int.Parse(displayNumber);
             }
         }
-        
+        //Console.WriteLine(CutTag("야야<야>호호"));
+
+        //// 가져온 text 내부의 태그 제거 함수
+        //string CutTag(string input)
+        //{
+        //    return Regex.Replace(input, @"<(.|\n)*?>", string.Empty);
+        //}
+
     }
 }
--- library_management_NaverAPI/Program.cs
+++ library_management_NaverAPI/Program.cs
@@ -6,10 +6,11 @@ class Program
     static void Main(string[] args)
     {
         NaverAPI naverAPI = new NaverAPI();
+        Console.WriteLine("검색어를 입력하세요.");
         string input = Console.ReadLine();
         Console.WriteLine($">>{input}<<");
-        naverAPI.NaverSearchBook(input);
+        Console.WriteLine("검색 결과를 몇 개씩 볼 지를 입력하세요.");
+        string number = Console.ReadLine();
+        naverAPI.NaverSearchBook("title", input, number);
     }
 }
-//test
-//test
